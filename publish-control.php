59b52a085a9776b6e7b3de54cf93e2b1da4bbf90
Matthew F. Fox <matt.f.fox@gmail.com>
2017-12-24 10:01:33 -0500

    Initial Commit.

---
+++ publish-control.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Publish Control
+Plugin URI: https://publishcontrol.foundry81.com
+Description: Provides a confirmation dialog upon content publishing.
+Version: 1.0
+Author: Matthew F. Fox
+Author URI: https://100781.org
+License: GPL2
+*/
